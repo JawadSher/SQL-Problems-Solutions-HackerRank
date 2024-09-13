e6342f119aaac933ea0ca38e91b025595f3f9d82
Jawad Sher <158135119+JawadSher@users.noreply.github.com>
2024-09-13 20:24:38 +0500

    Delete 02 - Advanced Select Problems/02 - The PADS directory

--- 02 - Advanced Select Problems/02 - The PADS/script.sql
+++
@@ -1,8 +0,0 @@
-SELECT CONCAT(NAME, '(',SUBSTR(Occupation, 1,1),')')
-FROM OCCUPATIONS
-ORDER BY NAME;
-
-SELECT CONCAT('There are a total of ', COUNT(*), ' ', LOWER(Occupation), 's.')
-FROM OCCUPATIONS
-GROUP BY Occupation
-ORDER BY COUNT(*), Occupation;
